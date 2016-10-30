be22de32227afafe0c5552ce43284f71045d1823
William Le Ferrand <william.le-ferrand@polytechnique.edu>
2016-10-29 19:17:10 -0700

    playing with the mpu driver

--- driver/main.c
+++ driver/main.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
 
+#include "MPU9250/MPU9250.h"
+
+const int i2c_bus = 1;
+
 int main() {
       printf("Accretio Wing Leveler\n");
+
+      mpu9250_init();
+
+      mpu9250_configure_i2c();
+
+      printf("Fully configured");
+
+      return 0;
+   
+
 }
