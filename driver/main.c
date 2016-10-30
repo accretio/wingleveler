917b5c01b4a2c5031a48f1bea0426fd548e26432
William Le Ferrand <william.le-ferrand@polytechnique.edu>
2016-10-29 22:42:16 -0700

    extracting some reference from the MPU 9250

--- driver/main.c
+++ driver/main.c
@@ -1,14 +1,75 @@
 #include<stdio.h>
+#include "linux_glue.h"
 
-const int i2c_bus = 1;
+#include <math.h>
+
 
 int main() {
       printf("Accretio Wing Leveler\n");
 
-     
-      printf("Fully configured");
+      linux_set_i2c_bus(1);
+      
+      if (mpu_init(NULL)) {
+            printf("\nmpu_init() failed\n");
+            return -1;
+      }    
 
-      return 0;
-   
+      unsigned short compass_sample_rate = 0; 
 
+      if (mpu_get_compass_sample_rate(&compass_sample_rate)) {
+            printf("couldn't get compass sample rate\n");
+            return -1;
+      }
+      printf("compass sample rate is %hu\n", compass_sample_rate);
+ 
+      unsigned short compass_fsr; 
+
+      if (mpu_get_compass_fsr(&compass_fsr)) {
+            printf("couldn't get compass fsr\n");
+      }
+
+      printf("compass fsr is %hu\n", compass_fsr);
+
+      if (mpu_set_sensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS)) {
+            printf("\nmpu_set_sensors() failed\n");
+            return -1;
+      }
+
+      if (mpu_configure_fifo(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS)) {
+            printf("\nmpu_configure_fifo() failed\n");
+            return -1;
+      }
+
+      if (mpu_set_sample_rate(10)) {
+            printf("\nmpu_set_sample_rate() failed\n");
+            return -1;
+      }
+
+      short compass_data[3] ;
+
+      while(1) {
+            if (mpu_get_compass_reg(compass_data, NULL)) {
+                  printf("couldn't get compass data\n");
+                  return -1;
+            }
+
+#define TWO_PI				(2.0f * (float)M_PI)
+
+            float newMagYaw = atan2f(compass_data[0], compass_data[1]);
+
+            if (newMagYaw != newMagYaw) {
+                  printf("newMagYaw NAN\n");
+                  return -1;
+            }
+
+            if (newMagYaw < 0.0f)
+                  newMagYaw = TWO_PI + newMagYaw;
+
+            newMagYaw = newMagYaw * 360 / TWO_PI; 
+
+            printf("yaw is %f\n", newMagYaw);
+            linux_delay_ms(100);
+      }
+
+    
 }
